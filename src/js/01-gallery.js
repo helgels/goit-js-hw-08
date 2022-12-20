bc191f2a7f2393a49b09e4381ce63267828c898b
Olia Rozhniatovska <ola.rozhnyatovska@gmail.com>
2022-12-20 10:39:26 +0200

    homework - done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+const GalleryMarkUp = createGalleryMarkUp(galleryItems);
+gallery.insertAdjacentHTML('beforeend', GalleryMarkUp);
+
+
+function createGalleryMarkUp(galleryItems) {
+  return galleryItems.map(
+    ({ preview, original, description }) => {
+      return `
+      <a class="gallery__item" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}" /></a>`;
+    })
+    .join("");
+}
+
+new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
