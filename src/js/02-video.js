0a935432df65f1bfe6999d386f4c422f4213c575
Olia Rozhniatovska <ola.rozhnyatovska@gmail.com>
2022-12-20 20:01:43 +0200

    corrected 2nd task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,13 +1,19 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
+const currentTime = "videoplayer-current-time";
+
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-
 player.on('timeupdate', throttle(onPlay, 1000));
-
-function onPlay({ seconds }) {
-  localStorage.setItem('videoplayer-current-time', seconds);
+   
+function onPlay ({ seconds }) {
+    localStorage.setItem(currentTime, seconds)
 }
-
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+setCurrentTime()
+function setCurrentTime(){
+    if(!localStorage.getItem(currentTime)){
+        return
+    }
+    player.setCurrentTime(localStorage.getItem(currentTime))
+}
